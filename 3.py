c6d06b3d573968adc3f89e8bc1a979128abb535d
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2024-02-26 09:14:58 +0530

    first commit

---
+++ 3.py
@@ -0,0 +1,10 @@
+class Glasses:
+    def printShadeIndex(self):
+        print("low")
+
+class Sunglasses(Glasses):
+    def printShadeIndex(self):
+        print("high")
+
+glasses=Glasses()
+glasses.printShadeIndex()    
